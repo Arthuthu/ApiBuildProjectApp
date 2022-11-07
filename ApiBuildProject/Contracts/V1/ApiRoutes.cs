8ba7f056f00a26093f35563a417d6fc17e67f81a
Arthuthu <arthurgeromello@hotmail.com>
2022-11-07 17:44:11 -0300

    API Versioning completed.

---
+++ ApiBuildProject/Contracts/V1/ApiRoutes.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ApiBuildProject.Contracts.V1
+{
+    public static class ApiRoutes
+    {
+        public const string Root = "api";
+
+        public const string Version = "v1";
+
+
+        public static class Users
+        {
+            public const string Route = $"{Root}/{Version}/[controller]";
+        }
+    }
+}
--- ApiBuildProject/Controllers/V1/UserController.cs
+++ ApiBuildProject/Controllers/V1/UserController.cs
@@ -1,10 +1,11 @@
-﻿using Microsoft.AspNetCore.Mvc;
+﻿using ApiBuildProject.Contracts.V1;
+using Microsoft.AspNetCore.Mvc;
 
 // For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
 
-namespace ApiBuildProject.Controllers
+namespace ApiBuildProject.Controllers.V1
 {
-    [Route("api/v1/[controller]")]
+    [Route(ApiRoutes.Users.Route)]
     [ApiController]
     public class UserController : ControllerBase
     {
